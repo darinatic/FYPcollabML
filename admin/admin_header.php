12621367c57615c7c8d4e2a8bac0a01ef0515dff
darinatic <123645376+darinatic@users.noreply.github.com>
2023-11-04 10:15:23 +0800

    Update admin_header.php

--- admin/admin_header.php
+++ admin/admin_header.php
@@ -6,23 +6,23 @@ if (session_status() == PHP_SESSION_NONE) {
 }
 
 // Check if the user is logged in and their role matches the allowed roles for this page
-if (isset($_SESSION['accountType'])) {
-    $userRole = $_SESSION['accountType'];
+// if (isset($_SESSION['accountType'])) {
+//     $userRole = $_SESSION['accountType'];
 
-    // Define the allowed roles for this page
-    $allowedRoles = array("System Admin");
+//     // Define the allowed roles for this page
+//     $allowedRoles = array("System Admin");
 
-    // Check if the user's role is allowed
-    if (!in_array($userRole, $allowedRoles)) {
-        // User has access, continue with the page
-        header("location: ../login.php"); // You can create an "access_denied.php" page
-        exit;
-    }
-} else {
-    // User is not logged in, redirect them to the login page
-    header("location: ../login.php");
-    exit;
-}
+//     // Check if the user's role is allowed
+//     if (!in_array($userRole, $allowedRoles)) {
+//         // User has access, continue with the page
+//         header("location: ../login.php"); // You can create an "access_denied.php" page
+//         exit;
+//     }
+// } else {
+//     // User is not logged in, redirect them to the login page
+//     header("location: ../login.php");
+//     exit;
+// }
 
 ?>
 
